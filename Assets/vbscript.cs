7d45e2471432ae1bd3b2e310f6c3aa19847db51a
Robert <RobertIndie@outlook.com>
2017-10-17 21:36:57 +0800

    Init project

---
+++ Assets/vbscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbscript : MonoBehaviour ,IVirtualButtonEventHandler{
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("按下");
+        player.transform.Translate(new Vector3(0, 0, 5));
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("松开");
+    }
+    public GameObject player;
+    public GameObject vbobject;
+    // Use this for initialization
+    void Start () {
+        vbobject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+	
+}
